0e2c8951c09a12d5adbd334cd6600bbc3b7bf9a3
ddabek <dallyshalla@gmail.com>
2016-03-18 02:36:00 +0000

    signature verification

--- examples/generate_keys.rs
+++ examples/generate_keys.rs
@@ -10,7 +10,7 @@ fn main() {
 	let the_secret = KeyPair::private_key_tobase64(our_key.secret);
 	print!("your base64 private key {:?} \n", the_secret);
 
-	let the_string = KeyPair::address_base58(our_key.public);
+	let the_string = KeyPair::address_base58(&our_key.public);
 	print!("your Hash160 Public Key: {:?} \n", the_string);
 
 
@@ -18,18 +18,11 @@ fn main() {
 	let the_secret = KeyPair::private_key_tobase64(the_keys.secret);
 	print!("your base64 private key {:?} \n", the_secret);
 
-	let the_string = KeyPair::address_base58(the_keys.public);
+	let the_string = KeyPair::address_base58(&the_keys.public);
 	print!("your Hash160 Public Key: {:?} \n", the_string);
 
 
-
 	let the_keys = KeyPair::from_secret(our_key.secret).unwrap();
-	//let the_secret = KeyPair::private_key_tobase64(the_keys.secret);
-	//print!("your base64 private key {:?} \n", the_secret);
-
-	//let the_string = KeyPair::address_base58(the_keys.public);
-	//print!("your Hash160 Public Key: {:?} \n", the_string);
-	//let this_strings = "hello".to_string();
 
 	let mut this_vec: Vec<u8> = Vec::new();
 	this_vec.push(099999);
@@ -38,6 +31,15 @@ fn main() {
 	let mut this_vec: Vec<u8> = Vec::new();
 	this_vec.push(099999);
 	let extract_pub = KeyPair::recover(our_signature, this_vec);
-	let the_string = KeyPair::address_base58(extract_pub);
+	let the_string = KeyPair::address_base58(&extract_pub);
 	print!("your Hash160 Public Key: {:?} \n", the_string);
+
+
+	let mut this_vec: Vec<u8> = Vec::new();
+	this_vec.push(099999);
+	let our_signature = KeyPair::sign(&the_keys.secret, this_vec);
+	let mut this_vec: Vec<u8> = Vec::new();
+	this_vec.push(099999);
+	let verified = KeyPair::verify(&extract_pub, our_signature,this_vec);
+	print!("Verification status: {:?}\n", verified);
 }
